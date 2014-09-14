6ec6a9fd44baaf9c99cf7f58518a4221d987c4e9
skullbocks <dgikille@gmail.com>
2014-09-14 02:58:47 +0200

    Added a demo file for the module

---
+++ demo.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+plugin 'jquery';
+
+get '/' => 'index';
+app->start;
+
+__DATA__
+@@ index.html.ep
+  <!doctype html>
+  <html>
+    <head>
+    	<meta charset="utf-8">
+  	<title>class demo</title>
+  	<style>
+  		div, span {
+    			width: 120px;
+    			height: 40px;
+    			float: left;
+    			padding: 10px;
+    			margin: 10px;
+    			background-color: #EEEEEE;
+		  }
+  	</style>
+      	%= asset "jquery.js"
+    </head>
+    <body>
+      	<div class="notMe">div class="notMe"</div>
+	<div class="myClass">div class="myClass"</div>
+	<span class="myClass">span class="myClass"</span>
+	<script>
+		$( ".myClass" ).css( "border", "3px solid red" );
+	</script>
+    </body>
+  </html>
