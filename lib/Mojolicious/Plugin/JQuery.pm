91a4143e8b22f36eaf625c84ba345b4c4d7b9058
Francesco Ferro <skullbocks@sabayon.org>
2015-11-05 14:38:15 +0100

    Released version 2.14001
    	* Changed AssetPack required version

--- lib/Mojolicious/Plugin/JQuery.pm
+++ lib/Mojolicious/Plugin/JQuery.pm
@@ -8,7 +8,7 @@ Mojolicious::Plugin::JQuery - Mojolicious + http://jquery.com/
 
 =head1 VERSION
 
-2.14000
+2.14001
 
 =head1 DESCRIPTION
 
@@ -95,7 +95,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use File::Spec::Functions 'catdir';
 use Cwd ();
 
-our $VERSION = '2.14000';
+our $VERSION = '2.14001';
 
 =head1 METHODS
 
